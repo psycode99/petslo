da887fc8d5b2db93ecdd811619da1e57a20b8ba9
psycode99 <georgefavour694@gmail.com>
2022-11-04 14:30:40 -0100

    working on landing page

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_required, LoginManager, current_user, login_user, logout_user
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from flask_gravatar import Gravatar
+
 from werkzeug.security import generate_password_hash, check_password_hash
 import config
 
@@ -12,8 +15,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = config.database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-class Users(db.Model):
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
+
+
+class Users(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False, unique=False)
@@ -36,13 +51,14 @@ class Posts(db.Model):
 db.create_all()
 
 
-
-
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', logged_in=current_user.is_authenticated, current_user=current_user)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -56,7 +72,12 @@ def login():
         if verify_username in all_users:
             checked_password = check_password_hash(verify_username.password, password)
             if checked_password:
-                return redirect(url_for('home'))
+                name = verify_username.name
+                user_n = verify_username.username
+                bio = verify_username.bio
+                user_pic = verify_username.user_image
+                login_user(verify_username)
+                return redirect(url_for('dashboard', username=user_n, bio=bio, name=name, user_pic=user_pic))
             else:
                 error = 'Invalid Password'
                 return redirect(url_for('login', error=error))
@@ -87,8 +108,17 @@ def signup():
         new_user = Users(name=name, email=email, username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('login'))
+        return redirect(url_for('login', user=new_user))
     return render_template('signup.html', error=error)
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    bio = request.args.get('bio')
+    user = request.args.get('username')
+    user_pic = request.args.get('user_pic')
+    name = request.args.get('name').title()
+    return render_template('dashboard.html', username=user, bio=bio, name=name, user_pic=user_pic)
 
 
 if __name__ == '__main__':
